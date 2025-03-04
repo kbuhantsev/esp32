cecd546f743926a3299705d96ba7ba591d79be31
kbuhantsev <k.buhantsev@gmail.com>
2025-03-04 13:17:25 +0200

    Update test_settings.ino

--- test_settings/test_settings.ino
+++ test_settings/test_settings.ino
@@ -1,8 +1,5 @@
 #include <Arduino.h>
 
-#define WIFI_SSID "STVOL_OFFICE"
-#define WIFI_PASS "stvolodessa"
-
 #include <GyverDBFile.h>
 #include <LittleFS.h>
 #include <SettingsGyver.h>
@@ -15,44 +12,63 @@ SettingsGyver sett("My Settings", &db);
 
 // имена ячеек базы данных
 DB_KEYS(
-    kk,
-    input
-);
+  kk,
+  input);
+
+int slider;
 
 // билдер! Тут строится наше окно настроек
 void build(sets::Builder& b) {
-    b.Input(kk::input, "My input");
+  b.Input(kk::input, "My input");
+  b.Slider("My slider", 0, 4096, 1, "", &slider);
+  
+  if (b.Button("Button")) {
+    Serial.print("click: ");
+    Serial.println(b.build.pressed());
+  }
+
+  if (b.ButtonHold("Button Hold")) {
+    Serial.print("hold: ");
+    Serial.println(b.build.pressed());
+  }
+
 }
 
 void setup() {
-    Serial.begin(115200);
-    Serial.println();
+  Serial.begin(115200);
+  Serial.println();
 
-    WiFi.mode(WIFI_AP_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PASS);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println();
-    Serial.print("Connected: ");
-    Serial.println(WiFi.localIP());
+  WiFi.mode(WIFI_AP_STA);
 
-#ifdef ESP32
-    LittleFS.begin(true);
-#else
-    LittleFS.begin();
-#endif
+  sett.begin();
+  sett.onBuild(build);
 
-    sett.begin();
-    sett.onBuild(build);
+  #ifdef ESP32
+  LittleFS.begin(true);
+  #else
+  LittleFS.begin();
+  #endif
 
-    // запуск и инициализация полей БД
-    db.begin();
-    db.init(kk::input, 0);
+  // запуск и инициализация полей БД
+  db.begin();
+  db.init(kk::input, 0);
+
+  // ======= AP =======
+  WiFi.softAP("AP ESP32");
+  Serial.print("AP IP: ");
+  Serial.println(WiFi.softAPIP());
+ 
 }
 
 void loop() {
-    // тикер, вызывать в лупе
-    sett.tick();
+  // тикер, вызывать в лупе
+  sett.tick();
+
+  static uint32_t tmrs;
+  if (millis() - tmrs >= 500) {
+    tmrs = millis();
+    static int i;
+    i++;
+    //Serial.println(slider);
+  }
 }
